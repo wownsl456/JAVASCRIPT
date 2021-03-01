32cb02cd308adfaf5415089b5f1c92544a84c3c8
wownsl456 <wownsl456@naver.com>
2021-03-01 21:01:24 +0900

    JAVA_SCRIPT

---
+++ JAVA_SCRIPT2/KeyboardEventPratice2/index.js
@@ -0,0 +1,23 @@
+const input = document.querySelector('#input');
+
+function checker() {
+  const words = document.querySelectorAll('.word');
+  if (words.length === 0) {
+    alert('Success!👏');
+    if(confirm('retry?')) {
+      window.location.reload();
+    }
+  }
+}
+
+// 여기에 코드를 작성해 주세요.
+function removeWord(){
+  const word = document.querySelector(`[data-word="${input.value}"]`);
+  if (word){
+  word.remove();
+  checker();
+  }
+  input.value = '';
+}
+
+input.addEventListener('change', removeWord);
