d56f565ccbd5a7ddb7876f58a131ca30dd17cdf4
wownsl456 <wownsl456@naver.com>
2021-03-20 23:20:41 +0900

    JAVA_SCRIPT

---
+++ JAVA_SCRIPT_Module/Pratice5/index.js
@@ -0,0 +1,9 @@
+import { addBtn, addInput, list, rollBtn } from './tags.js';
+// 여기에 코드를 작성해 주세요.
+import { addMenu, deleteMenu, rollMenu } from './functions.js';
+const data = [];
+
+addBtn.addEventListener('click', () => addMenu(data));
+addInput.addEventListener('keypress', (e) => e.code === 'Enter' && addMenu(data));
+list.addEventListener('click', ({ target }) => target.tagName === 'BUTTON' && deleteMenu(data, target.dataset.index));
+rollBtn.addEventListener('click', () => list.classList.contains('rolling') || rollMenu(data));
